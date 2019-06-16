0c89d999c7071b5a08426966147875c7e8a63a85
Anup Kanoor Gopalakrishnan <agopalakrishnan@manh.com>
2019-06-16 13:52:25 -0400

    kk

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args)
 	{
-		System.out.println("hello");
+		System.out.println("hello y");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
